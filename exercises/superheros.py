95c87ccfb9f8686dc7deaf742a8a9f543660d601
majd19-meet <majd19@meet.mit.edu>
2018-08-05 18:31:54 +0300

    yo

--- exercises/superheros.py
+++ exercises/superheros.py
@@ -1,1 +1,11 @@
 # Write your solutions for 1.5 here!
+class Superheros:
+	def __init__(self, name, superpower, strength):
+		self.name = name
+		self.superpower = superpower
+		self.strength = strength
+
+	def Chandler Bing (self, chanchan man, sarcasm, 100):
+		print ("My name is", self.name)
+
+
--- exercises/while_loops.py
+++ exercises/while_loops.py
@@ -1,1 +1,7 @@
 # Write your solution for 1.3 here!
+n = 0
+i = 1
+while i < 10000:
+	n = n + i
+	i = i + 1
+print(i)
